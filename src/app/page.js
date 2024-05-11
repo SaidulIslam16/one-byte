18236cdd6917963e69e872a878e41eae303f4650
SaidulIslam16 <isaidulislamsayed@gmail.com>
2024-05-11 10:26:54 +0600

    responsive css added and make some changes in layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        obcaecati suscipit at ex amet laborum voluptas possimus? Quis, mollitia
+        eligendi?
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid,
+        incidunt voluptates, explicabo iusto excepturi beatae, perspiciatis
+        fugiat sint eos eveniet ab. Distinctio fugit ipsam laborum quo eius
+        suscipit perferendis nulla dicta nam autem sunt reiciendis assumenda
+        aliquid corporis iste sed dolores velit sint maiores cumque tempora
+        ipsum, libero excepturi. Temporibus, quaerat perspiciatis hic cum
+        nostrum illo maxime rem ab molestias molestiae! Perferendis autem
+        accusamus impedit repudiandae assumenda possimus temporibus repellendus
+        ipsam consequatur quos. Omnis voluptatibus recusandae nemo unde aliquid
+        illo reiciendis, ut pariatur maxime sint quaerat commodi eum. Vitae
+        molestiae similique tempore consectetur. Eum autem non sapiente deleniti
+        architecto?
+      </p>
+    </main>
+  );
 }
